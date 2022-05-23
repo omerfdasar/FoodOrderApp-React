580f9cea2ea30066f12f98ca24285e0c53d1b6eb
Omer <omerfarukdasar@gmail.com>
2022-05-23 18:54:02 +0200

    modal enhanced

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,16 +2,29 @@ import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import { useContext } from "react";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)}`;
 
   const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const carItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item, index) => (
-        <li key={index}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={carItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
