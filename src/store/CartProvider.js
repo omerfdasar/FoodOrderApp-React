d628cad00284e87ec2c25450db9cfac6a20d876e
Omer <omerfarukdasar@gmail.com>
2022-07-06 10:33:25 +0200

    context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -57,6 +57,9 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
   return defaultCartState;
 };
 
@@ -73,7 +76,9 @@ const CartProvider = (props) => {
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
-
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
